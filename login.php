4720d81340f815e9462bcdaea7f7f7d198ce99d4
Jhonatancs <jdc_santos@outlook.com>
2019-11-01 00:14:01 -0300

    corrigindo erro de nome de funcao

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php 
 	require_once 'functions.php';
 
-	if(isset($_GET['user']) && isset($_GET['pw'])){
+	if(isset($_POST['user']) && isset($_POST['pw']) ){
 		
-		$usuarios = listaAdmin($_GET['user'],$_GET['pw']);
+		$usuarios = listarAdmin($_POST['user'],$_POST['pw']);
 
 		if(is_array($usuarios) && count($usuarios) == 1){
 			$res = gerarHashLogin($usuarios[0]);
