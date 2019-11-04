ae59e247c0ef3aa8720c018fa6c0f0675b5d0f3e
Jhonatancs <jdc_santos@outlook.com>
2019-11-03 21:54:04 -0300

    fazendo alguns testes mirabolantes

--- login.php
+++ login.php
@@ -10,16 +10,16 @@
 	}
 
 
-	// if(isset($_POST['user']) && isset($_POST['pw']) ){
+	if(isset($_POST['user']) && isset($_POST['pw']) ){
 		
-	// 	$usuarios = listarAdmin($_POST['user'],$_POST['pw']);
+		$usuarios = listarAdmin($_POST['user'],$_POST['pw']);
 
-	// 	if(is_array($usuarios) && count($usuarios) == 1){
-	// 		$res = gerarHashLogin($usuarios[0]);
-	// 		echo json_encode($res);
-	// 	}else{
-	// 		$json['status'] = 0;
-	// 		$json['msg'] = "Usuário ou senha incorretos";
-	// 		echo json_encode($json);
-	// 	}
-	// }
+		if(is_array($usuarios) && count($usuarios) == 1){
+			$res = gerarHashLogin($usuarios[0]);
+			echo json_encode($res);
+		}else{
+			$json['status'] = 0;
+			$json['msg'] = "Usuário ou senha incorretos";
+			echo json_encode($json);
+		}
+	}
