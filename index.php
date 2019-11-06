ff7e6174306975f6edeaf85fe56cd68448bbb963
Jhonatancs <jdc_santos@outlook.com>
2019-11-06 01:25:37 -0300

    corrigindo funcao de editar na function.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 Olar!
 <meta charset="utf-8">
-<form method="post" action="criar.php">
-	<input type="" name="criar" value="prod"><br>
+<form method="post" action="editar.php">
+	<input type="" name="editar" value="prod"><br>
 	<input type="" name="nome" value="prod1"><br>
 	<input type="" name="qtd" value="1"><br>
 	<input type="" name="status" value="1"><br>
 	<input type="" name="valor" value="12"><br>
 	<input type="" name="cod_barras" value="123123123"><br>
-
+	<input type="" name="cd" value="3"><br>
 
 	<input type="submit" value="enviar"><br>
 </form>
